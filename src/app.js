bed9ed15fd2eda938effeedf7215653f2db5e7ec
Shin Maung Maung <ethereal97@gmail.com>
2022-05-25 21:05:16 +0630

    added cors library

--- src/app.js
+++ src/app.js
@@ -1,22 +1,25 @@
-const { IS_DEV, SERVER_PORT } = require('./config');
-const express = require('express');
+const { IS_DEV, SERVER_PORT } = require("./config");
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 if (IS_DEV) {
-  console.log('env:development');
-  app.use(require('morgan')('dev'));
+  console.log("env:development");
+  app.use(require("morgan")("dev"));
 } else {
-  console.log('env:production');
+  console.log("env:production");
 }
 
 app.use(express.static(`${__dirname}/../public`));
 
+app.use(cors());
+
 app.use(express.json());
 
 app.listen(SERVER_PORT, () =>
-  console.log('> server is running on port: %s', SERVER_PORT)
+  console.log("> server is running on port: %s", SERVER_PORT)
 );
 
 module.exports = app;
